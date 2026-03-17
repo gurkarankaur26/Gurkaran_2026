89b9bee75b37a1bbd4c33acd37d82f78cdacea3f
Gurkaran Kaur <gurkaran.kaur@gmail.com>
2026-03-17 06:06:42 +0530

    Updated

--- Assignment_8Mar2026/src/product_search_engine.py
+++ Assignment_8Mar2026/src/product_search_engine.py
@@ -26,84 +26,44 @@ client = openai.OpenAI(
     api_key=openai_key,
 )
 
-#  Product Dataset
-# data = {
-#      "Cargo Pants": "Durable pants with multiple pockets for functionality and style" ,
-#       "6-Pocket Pants": "Pants with two front, two back, and two side pockets.",
-#       "Cargo Joggers": "Pant that ombines the multi-pocket style with an elasticized waistband and cuffs.",
-#       "Tactical Cargo Pants": "Pants designed for durability, often featuring specialized pockets and, at times, zippers.",
-#      "Leather Crossbody Bag": "A sustainable, compact bag with an adjustable strap",
-#      "Polarized Sunglasses": "Stylish eyewear that reduces glare and protects eyes from UV rays",
-#      "Plant Grow Bags": "Breathable fabric pots for gardening on balconies or patios",
-#      "Water Pump Sprayer": "A handheld tool for watering plants or applying fertilizer."
-# }
-# Set the path to the file you'd like to load
-# download dataset
+#  Product Dataset:# download dataset
 path = kagglehub.dataset_download(
    "PromptCloudHQ/flipkart-products"
 )
-
-# p1 = r"C:\Users\Lenovo\.cache\kagglehub\datasets\PromptCloudHQ\flipkart-products\versions\1"
-# print(os.listdir(p1))
-
-# p2 = os.path.join(p1, "home")
-# print(os.listdir(p2))
-
-# p3 = os.path.join(p2, "sdf")
-# print(os.listdir(p3))
-
-
-
 file_path = os.path.join(
     path,
     "flipkart_com-ecommerce_sample.csv"
 )
-
 df = pd.read_csv(file_path)
-
 continue_input = "Y"
 while (continue_input.upper() =="Y" or continue_input.upper() == "YES"):
     # Set User Input
     user_description = input("Please enter the product you are looking for:  ")
-
-    user_vec = get_embedding(user_description)
-
-    # Use head() to get the first 1000 rows
-    #data = df.head(100)
-    
-   
+    user_vec = get_embedding(user_description)    
+    #Filter the dataset as per the user input
     data = df[df["description"].str.contains(user_description, case=False, na=False)]   
-    if not data.empty:        
+    if not data.empty:      
         #  Create Database dictionary
         product_vectors = {}
-
-        #  Create Database dictionary
-        product_vectors = {}
-
         for name, description in zip(data["product_name"], data["description"]):
-
             text = f"{name}. Category: {description}"
-
             embedding = get_embedding(text)
-
-            product_vectors[name] = embedding
+            product_vectors[name] = embedding     
             
-
-
-
         # Find Closest products
         similarities = {}
         for product, vec in product_vectors.items():
             # Calculate similarity and store in the dictionary
             sim = cosine_similarity([user_vec], [vec])[0][0]# fetch the finl similarity score from the first row and first elemen
             similarities[product] = sim
-        #print top three similar products
+            
+        #sorted the products in the descending order of cosine similarity
         sorted_products = sorted(
             similarities.items(),
             key=lambda x: x[1],
             reverse=True
         )
-    
+         #print top three similar products
         for product, score in sorted_products[:3]:
                 print(product, score)
      
